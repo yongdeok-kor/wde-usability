f6aed69acd63dfa7c20dc025ed97c84e64366e4f
yongdeok-kor <yunyd1234@korea.ac.kr>
2023-10-18 21:22:44 +0900

    Update system_interface.lisp

--- actr7.x/usability/system_interface.lisp
+++ actr7.x/usability/system_interface.lisp
@@ -16,9 +16,9 @@
 ;;
 ;;;;;;;;;;;;
 
-(actr-load "C:/Users/yongdeok/Documents/GitHub/wde-usability/actr7.x/usability/1.0/model_parameters.lisp")
-(actr-load "C:/Users/yongdeok/Documents/GitHub/wde-usability/actr7.x/usability/1.0/model/simple_model.lisp")
-(actr-load "C:/Users/yongdeok/Documents/GitHub/wde-usability/actr7.x/usability/1.0/task/simple_arm_movement_task.lisp")
+(actr-load "actr7.x/usability/1.0/model_parameters.lisp")
+(actr-load "actr7.x/usability/1.0/model/simple_model.lisp")
+(actr-load "actr7.x/usability/1.0/task/simple_arm_movement_task.lisp")
 
 
 ;;;
