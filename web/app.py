8bf6952f92dce5f7f892650f16bbf3156e35bc2d
Donghee Park <dongheepark@gmail.com>
2023-11-10 16:50:23 +0900

    Add error rate from control code

--- web/app.py
+++ web/app.py
@@ -13,16 +13,18 @@ def index():
     device = 'upper' if request.args.get('device') == None else request.args.get('device')
     body = 'body1' if request.args.get('body') == None else request.args.get('body')
     joint = 'joint1' if request.args.get('joint') == None else request.args.get('joint')
-    url = f'/?device={device}&body={body}&joint={joint}'
+    code = 'code1' if request.args.get('code') == None else request.args.get('code')
+    url = f'/?device={device}&body={body}&joint={joint}&code={code}'
     age = 30
-    return render_template('index.html', url=url, device=device, body=body, age=age)
+    return render_template('index.html', url=url, device=device, body=body, age=age, code=code)
 
 @app.route('/information')
 def information():
     device = request.args.get('device')
     body = request.args.get('body')
     task = request.args.get('task')
-    return render_template('information.html', device=device, body=body, task=task)
+    code = request.args.get('code')
+    return render_template('information.html', device=device, body=body, task=task, code=code)
 
 @app.route('/score/usability')
 def interactivity():
@@ -35,7 +37,15 @@ def interactivity():
         task = "arm" # default
  
     age = request.args.get('age')
+    code = request.args.get('code')
+
     error_rate = 0
+    if code == 'code1':
+        error_rate = 10
+    elif code == 'code2':
+        error_rate = 35
+    elif code == 'code3':
+        error_rate = 80
 
     return f"""
     <div id="usability-result" hx-swap-oob="true" hx-swap="outerHTML">
@@ -55,7 +65,7 @@ def interactivity():
 def fig_inter_force(task, age, error_rate):
     timeout = 10
     age = int(age)
-    force = 5
+    force = int(error_rate)
     noise = 5
     img = draw_usability_graph(timeout, [task, "adaptive"], age, force, noise)
     #  img = draw_usability_graph(10, ["gait", "adaptive"], 30, 5, 5)
